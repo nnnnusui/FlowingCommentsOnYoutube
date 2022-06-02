d693bf807021c475d0cc2ae0837ecf11725d4363
nnnnusui <nnnnusui@gmail.com>
2022-06-02 13:59:29 +0000

    🛠️: use vite, solidjs
    remove webpack stylus

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+import solidPlugin from 'vite-plugin-solid';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: "src/manifest.json"
+        }
+    },
+    plugins: [
+        chromeExtension(),
+        solidPlugin()
+    ],
+})
